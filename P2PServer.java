a55e9486b11da3d3125aff4b9b8eeb96b7b1dea1
William Wong <bill.ww17@gmail.com>
2019-04-09 13:16:38 -0400

    Update P2PServer.java

--- P2PServer.java
+++ P2PServer.java
@@ -9,7 +9,8 @@ import javax.imageio.ImageIO;
  * the directory server of what jpg it has. It then waits for any p2pclient to
  * connect to it and request a jpg
  * 
- * @author Andrew Benson 500745614
+ * @author Andrew Benson (500745614), William Wong(500754922), Aravinth
+ * 
  *
  */
 public class P2PServer {
@@ -25,15 +26,15 @@ public class P2PServer {
 	 */
 	public static void main(String[] args) throws Exception {
 
-		String[] jpgList = { "pingu.jpg" };
-		// Send name of jpg to Directory servers via UDP
+		 // Send name of jpg to Directory servers via UDP
 		 InformAndUpdate("pingu.jpg", 20170);
-		 InformAndUpdate("NOOT.jpg", 20170);
+		 
 		 //Do another inform and update for a second picture
-
+		 InformAndUpdate("NOOT.jpg", 20170);
+		 
 		// Wait for Client to connect via TCP
-		TCPServer(jpgList, 20175);
-		TCPServer(jpgList, 20175);
+		TCPServer(20171);
+		TCPServer(20171);
 	}
 
 	/**
@@ -43,34 +44,29 @@ public class P2PServer {
 	 * @param port the port number used to connect
 	 * @throws IOException
 	 */
-	static void InformAndUpdate(String jpg, int port) throws IOException {
+	static void InformAndUpdate(String jpg, int port) throws IOException 
+	{
 		// Send name of jpg to Directory servers via UDP
-
-		// BufferedReader inFromUser = new BufferedReader(new
-		// InputStreamReader(System.in));
-System.out.println("Informing Directory Server...");
+		System.out.println("Informing Directory Server...");
 		DatagramSocket clientSocket = new DatagramSocket();
 
 		//change to ip of directory server
+		InetAddress IPAddress = InetAddress.getByName("ENG201-05");
 		
-//		byte [] ipAddr = new byte[]{(byte) 141,117,(byte) 232,101};
-//		InetAddress IPAddress = InetAddress.getByAddress(ipAddr);
-		InetAddress IPAddress = InetAddress.getByName("ENG202-42");
-		System.out.println(IPAddress.getHostAddress());
-		//InetAddress IPAddress = InetAddress.getByAddress("127.0.0.1".getBytes());
-		//System.out.println(IPAddress);
-
+		//local host testing
+		//InetAddress IPAddress = InetAddress.getByName("localhost");
+		//System.out.println(IPAddress.getHostAddress());
+		
 		byte[] sendData = new byte[1024];
 
 		// Type input
-		// String sentence = inFromUser.readLine();
 		String data = "1:" + jpg;
 		sendData = data.getBytes();
 
 		// Create Datagram with data, length, ip addr, port
 		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
 		clientSocket.send(sendPacket);
-System.out.println("Directory Server Informed");
+		System.out.println("Directory Server Informed");
 		clientSocket.close();
 	}
 
@@ -82,7 +78,7 @@ System.out.println("Directory Server Informed");
 	 * @param port
 	 * @throws Exception
 	 */
-	static void TCPServer(String[] jpgList, int port) throws Exception {
+	static void TCPServer(int port) throws Exception {
 		// Create server socket
 
 		ServerSocket ss = new ServerSocket(port);
@@ -97,36 +93,38 @@ System.out.println("Directory Server Informed");
 		// create input reader and print writer
 		InputStreamReader in = new InputStreamReader(s.getInputStream());
 		BufferedReader bf = new BufferedReader(in);
-		PrintWriter pr = new PrintWriter(s.getOutputStream());
 
-		while (true) {
-
-			try {
+		while (true) 
+		{
+			try 
+			{
 
 				// Read what client is requesting
 				String jpgName = bf.readLine();
 				System.out.println("Client: " + jpgName);
-				// Send jpb with that name
-				// pr.println("I have:" + str);
-				// pr.flush();
-
-				// Get Jpg
 
 				// Location of file on computer
-				bimg = ImageIO.read(new File(
-						"H:\\CPS 706\\"+jpgName));
+				bimg = ImageIO.read(new File("H:\\CPS 706\\"+jpgName));
 				ImageIO.write(bimg, "JPG", s.getOutputStream());
 				ImageIO.write(bimg, "JPG", s.getOutputStream());
 				System.out.println("Image sent!!!!");
-
 				s.getOutputStream().flush();
-			} catch (SocketException st) {
+			}
+			//if socket is already closed
+			catch (SocketException st) 
+			{
 				System.out.println("Socket Closed!");
 				break;
-			} catch (SocketTimeoutException st) {
+			} 
+			//if socket timed out
+			catch (SocketTimeoutException st)
+			{
 				System.out.println("Socket timed out!");
 				break;
-			} catch (Exception ex) {
+			}
+			// literally any other thing
+			catch (Exception ex) 
+			{
 				System.out.println(ex);
 				break;
 			}
